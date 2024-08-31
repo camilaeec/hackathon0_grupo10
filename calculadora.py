f7ad87fd695d67c47272c6a1ed0cdee0b5ee7e06
CamilaE <camila.espinoza@utec.edu.pe>
2024-08-31 16:58:07 -0500

    resta

---
+++ calculadora.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+        
+        elif opcion == '2':
+            num1 = float(input("Ingresa el primer número: "))
+            num2 = float(input("Ingresa el segundo número: "))
+            print(f"Resultado: {num1} - {num2} = {num1 - num2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
